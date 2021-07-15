32a4c459d4b01e9571304f345b3b5918a1957905
Daniel Chagas <daniel.chagas@runtalent.it>
2021-07-15 11:01:21 -0300

    inicial

---
+++ console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var palavra = "ovo";
+
+            var resultado = Eh(palavra);
+
+            Console.WriteLine(resultado);
+        }
+
+        static bool Eh(string palavra)
+        {
+            var lista = palavra.ToList();
+            
+            while(lista.Count() > 0)
+            {
+                if(lista.Count() == 1) break;
+
+                if(lista.First() == lista.Last())
+                {
+                    lista.RemoveAt(0);
+                    lista.RemoveAt(lista.Count() - 1);
+                }
+                else return false;
+            }
+
+            return true;
+        }
+    }
+}
